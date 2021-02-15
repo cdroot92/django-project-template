f8da764088e754459ae135c5237d912426c5ae9d
cdroot92 <cdroot92@gmail.com>
2021-02-16 02:08:34 +0900

    Change project name for being used template

--- project_name/asgi.py
+++ project_name/asgi.py
@@ -14,6 +14,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{project_name}}.settings")
 
 application = get_asgi_application()
--- project_name/settings.py
+++ project_name/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "project.urls"
+ROOT_URLCONF = "{{project_name}}.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "project.wsgi.application"
+WSGI_APPLICATION = "{{project_name}}.wsgi.application"
 
 
 # Database
