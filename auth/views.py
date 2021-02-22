1c808f6bef36d8732b012bb573ef3d4db75f6b23
cdroot92 <cdroot92@gmail.com>
2021-02-23 01:48:01 +0900

    Add ListUsers view

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .models import User
+
+
+class ListUsers(APIView):
+    authentication_classes = [JWTAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
